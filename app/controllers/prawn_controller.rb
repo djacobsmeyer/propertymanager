dee87c985c39f7634c5e86662f80a583927674e2
Don Jacobsmeyer <donjacobsmeyer@gmail.com>
2015-09-18 15:05:17 -0500

    commented out Prawn#create

--- app/controllers/prawn_controller.rb
+++ app/controllers/prawn_controller.rb
@@ -6,15 +6,15 @@ class PrawnController < ApplicationController
   end
 
   def create
-    Prawn::Document.generate("invoice#{DateTime.now}.pdf") do |pdf|
-      render  layout: "/layouts/prawn/receivable" # text/formating to be made into pdf
-      if pdf.save
-        # do something
-        redirect_to :receivable
-      else
-        [flash] :error("PDF could not be created")
-        render :receivable
-    end
+    # Prawn::Document.generate("invoice#{DateTime.now}.pdf") do |pdf|
+    #   render  layout: "/layouts/prawn/receivable" # text/formating to be made into pdf
+    #   if pdf.save
+    #     # do something
+    #     redirect_to :receivable
+    #   else
+    #     [flash] :error("PDF could not be created")
+    #     render :receivable
+    # end
 
   end
 
